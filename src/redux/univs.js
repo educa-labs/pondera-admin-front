8b4723f930372b0ae230c673e50604c284052514
Ivan Wolf <ivanwolf15@gmail.com>
2017-12-24 21:14:55 -0300

    redux fetch duck

--- src/redux/univs.js
+++ src/redux/univs.js
@@ -1,70 +1,9 @@
-import { combineReducers } from 'redux';
+import { withFetch, thunkCreator } from 'redux-fetch-duck';
 import api from '../api';
 
-const GET_UNIVS_REQUEST = 'GET_UNIVS_REQUEST';
-const GET_UNIVS_SUCCESS = 'GET_UNIVS_SUCCESS';
-const GET_UNIVS_FAILURE = 'GET_UNIVS_FAILURE';
+const dataSelector = res => res.data.data;
 
-const getUnivsRequest = () => ({
-  type: GET_UNIVS_REQUEST,
-});
+export const getUnivs = thunkCreator('univs', api.getAllUnivs, dataSelector);
 
-const getUnivsSuccess = univs => ({
-  type: GET_UNIVS_SUCCESS,
-  univs,
-});
-
-const getUnivsFailure = error => ({
-  type: GET_UNIVS_FAILURE,
-  error,
-});
-
-export const getUnivs = token => (
-  async (dispatch) => {
-    dispatch(getUnivsRequest());
-    try {
-      const res = await api.getAllUnivs(token);
-      dispatch(getUnivsSuccess(res.data.data));
-    } catch (err) {
-      dispatch(getUnivsFailure(err));
-    }
-  }
-);
-
-
-const loading = (state = false, action) => {
-  switch (action.type) {
-    case GET_UNIVS_REQUEST:
-      return true;
-    case GET_UNIVS_FAILURE:
-    case GET_UNIVS_SUCCESS:
-      return false;
-    default:
-      return state;
-  }
-};
-
-const error = (state = null, action) => {
-  switch (action.type) {
-    case GET_UNIVS_FAILURE:
-      return action.error;
-    default:
-      return state;
-  }
-};
-
-const univs = (state = null, action) => {
-  switch (action.type) {
-    case GET_UNIVS_SUCCESS:
-      return action.univs;
-    default:
-      return state;
-  }
-};
-
-export default combineReducers({
-  univs,
-  loading,
-  error,
-});
+export default withFetch('univs')();
 
